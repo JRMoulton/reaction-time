ba61a35ee428aa4f00de12344025388bbc16246d
Jared Moulton <jaredmoulton3@gmail.com>
2018-06-19 00:43:58 -0600

    Added the javascript for motion and height

--- reaction.js
+++ reaction.js
@@ -3,7 +3,10 @@ var myObject = document.getElementById("object");
 
 
 
-let colors = ["black", "blue", "red", "cyan", "green", "navy", "silver"];
+let colors = ["black", "blue", "red", "cyan", "green"];
+var prevColor = "black";
+newColor = "black";
+heightWidth = 10;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -12,12 +15,17 @@ function getRndInteger(min, max) {
 function change() {
 
     console.log("Here");
-    myObject.style.top = getRndInteger(300, screen.height);
-    myObject.style.left = getRndInteger(300, screen.width);
+    myObject.style.top = getRndInteger(300, screen.height-500).toString() + "px";
+    myObject.style.left = getRndInteger(300, screen.width-500).toString() + "px";
 
-    myObject.style.height = Math.floor(getRndInteger(50, 500));
-    myObject.style.width = myObject.style.height;
-    console.log(myObject)
+    heightWidth = Math.floor(getRndInteger(50, 300)).toString() + "px";
+    myObject.style.height = heightWidth;
+    myObject.style.width = heightWidth;
 
-    myObject.style.color = colors[getRndInteger(0, colors.length)]
+    while (prevColor == newColor) {
+        newColor = colors[getRndInteger(0, colors.length)];
+    }
+    prevColor = newColor;
+    myObject.style.background = newColor;
+    console.log(myObject.style.top);
 }
