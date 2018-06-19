f5aa1c4ccf2550fc9106ccfb914efa8c75c64959
Jared Moulton <jaredmoulton3@gmail.com>
2018-06-19 11:54:05 -0600

    Finished all random changes

--- reaction.js
+++ reaction.js
@@ -7,6 +7,7 @@ let colors = ["black", "blue", "red", "cyan", "green"];
 var prevColor = "black";
 newColor = "black";
 heightWidth = 10;
+sqCir = 0;
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -15,17 +16,23 @@ function getRndInteger(min, max) {
 function change() {
 
     console.log("Here");
-    myObject.style.top = getRndInteger(300, screen.height-500).toString() + "px";
-    myObject.style.left = getRndInteger(300, screen.width-500).toString() + "px";
+    myObject.style.top = getRndInteger(300, screen.height - 500).toString() + "px";
+    myObject.style.left = getRndInteger(300, screen.width - 500).toString() + "px";
 
     heightWidth = Math.floor(getRndInteger(50, 300)).toString() + "px";
     myObject.style.height = heightWidth;
     myObject.style.width = heightWidth;
+
+   sqCir = getRndInteger(0,2);
+   if (sqCir == 0) {
+       myObject.style.borderRadius = "0%";
+   } else {
+       myObject.style.borderRadius = "50%";
+   }
 
     while (prevColor == newColor) {
         newColor = colors[getRndInteger(0, colors.length)];
     }
     prevColor = newColor;
     myObject.style.background = newColor;
-    console.log(myObject.style.top);
 }
