c4e6a518e9a2f1fa8430bc5aec320a3f87cef08c
Jared Moulton <jaredmoulton3@gmail.com>
2018-06-19 12:49:59 -0600

    Setup for the timers

---
+++ plans.md
@@ -0,0 +1,7 @@
+# Plans for the future
+
+Timer functionality that keeps track of each individual time and the average time. 
+
+When the end is reached report back the fastest response time and the slowest
+
+Also report back the average time
--- reaction.js
+++ reaction.js
@@ -1,13 +1,33 @@
 
 var myObject = document.getElementById("object");
-
-
+var myInput = document.getElementById("input");
+var body = document.getElementById("body");
+var mySubmit = document.getElementById("submit");
 
 let colors = ["black", "blue", "red", "cyan", "green"];
 var prevColor = "black";
+
 newColor = "black";
 heightWidth = 10;
 sqCir = 0;
+
+
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        mySubmit.click();
+    }
+});
+
+function getInput() {
+    let count = myInput.value;
+    myInput.value = "";
+    myInput.style.height = "0px";
+
+    body.removeChild(myInput);
+    body.removeChild(mySubmit);
+}
+
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
@@ -19,16 +39,16 @@ function change() {
     myObject.style.top = getRndInteger(300, screen.height - 500).toString() + "px";
     myObject.style.left = getRndInteger(300, screen.width - 500).toString() + "px";
 
-    heightWidth = Math.floor(getRndInteger(50, 300)).toString() + "px";
+    heightWidth = Math.floor(getRndInteger(15, 300)).toString() + "px";
     myObject.style.height = heightWidth;
     myObject.style.width = heightWidth;
 
-   sqCir = getRndInteger(0,2);
-   if (sqCir == 0) {
-       myObject.style.borderRadius = "0%";
-   } else {
-       myObject.style.borderRadius = "50%";
-   }
+    sqCir = getRndInteger(0, 2);
+    if (sqCir == 0) {
+        myObject.style.borderRadius = "0%";
+    } else {
+        myObject.style.borderRadius = "50%";
+    }
 
     while (prevColor == newColor) {
         newColor = colors[getRndInteger(0, colors.length)];
@@ -36,3 +56,5 @@ function change() {
     prevColor = newColor;
     myObject.style.background = newColor;
 }
+
+
